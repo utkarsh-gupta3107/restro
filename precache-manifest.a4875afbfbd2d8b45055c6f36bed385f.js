ef06e1855448a4664b01a999675314ec31fcb370
unknown <utkarshgupta6203@gmail.com>
2023-05-23 13:57:36 +0530

    Updates

---
+++ precache-manifest.a4875afbfbd2d8b45055c6f36bed385f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68734aa36ef62850022e",
+    "url": "/restro/static/css/main.ee871b33.chunk.css"
+  },
+  {
+    "revision": "68734aa36ef62850022e",
+    "url": "/restro/static/js/main.68734aa3.chunk.js"
+  },
+  {
+    "revision": "a99c263911670a2ba01f",
+    "url": "/restro/static/js/1.a99c2639.chunk.js"
+  },
+  {
+    "revision": "2ee1633a864f09eb90b7",
+    "url": "/restro/static/js/runtime~main.2ee1633a.js"
+  },
+  {
+    "revision": "3a20eba9bb150ebf870b63fd06562be3",
+    "url": "/restro/index.html"
+  }
+];
