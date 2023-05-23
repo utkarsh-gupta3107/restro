e95414c60802eb60b8943979d407535001003c51
unknown <utkarshgupta6203@gmail.com>
2023-05-23 19:49:15 +0530

    naan and change text

--- src/component/basics/menuApi.js
+++ src/component/basics/menuApi.js
@@ -4,56 +4,50 @@ const Menu = [
       image: "images/maggi.jpg",
       name: "maggi",
       category: "breakfast",
-      price: "12₹",
-      description:
-        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      price: "₹50",
+      description:"Maggi is a popular instant noodle dish loved for its quick and easy preparation. It's a comforting meal made with noodles, spices, and a flavorful seasoning that satisfies cravings in minutes.",
     },
   
     {
       id: 2,
       image: "images/allupakoida.jpg",
-      name: "allu pakoida",
+      name: "Alu pakoda",
       category: "evening",
-      price: "20₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      price: "₹80",
+      description:"Alu pakoda, also known as potato fritters, are crispy and flavorful Indian snacks made by deep-frying potato slices coated in a spiced gram flour batter. They are perfect for rainy days and tea time treats.",
     },
     {
       id: 3,
       image: "images/corn.jpg",
-      name: "corn",
+      name: "Corn",
       category: "breakfast",
-      price: "10₹",
+      price: "₹40",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"Bhutta, also known as roasted corn on the cob, is a popular street food in India. The corn is grilled over charcoal or open flame, brushed with butter, and seasoned with spices for a smoky and savory flavor.",    },
     {
       id: 4,
       image: "../images/chola.jpg",
-      name: "chola",
+      name: "Chole",
       category: "lunch",
-      price: "50₹",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      price: "₹100",
+      description:"Chole, a classic Punjabi dish, features tender chickpeas simmered in a rich and spicy tomato-based gravy, infused with aromatic spices. It pairs perfectly with fluffy bhaturas or rice for a satisfying meal.",
     },
     {
       id: 5,
       image: "../images/pizza.jpg",
-      name: "pizza",
+      name: "Pizza",
       category: "evening",
-      price: "80₹",
+      price: "₹150",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"Pizza, a renowned Italian delight, features a crust topped with tomato sauce, cheese, and a variety of toppings. It's a universally adored dish, combining flavors and textures in each mouthwatering slice.",    },
     {
       id: 6,
       image: "../images/nonvegthali.jpg",
       name: "Non-Veg Thali",
       category: "dinner",
-      price: "180₹",
+      price: "₹180",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"A non-veg thali is a flavorful platter with a range of meat dishes, including chicken, fish, or lamb, accompanied by rice, curries, breads, and assorted condiments, providing a delightful meaty feast.",    },
     {
       id: 7,
       image: "../images/sweet.jpg",
@@ -61,35 +55,31 @@ const Menu = [
       category: "dinner",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"Indian sweets, or mithai, are a delectable assortment of confections with rich flavors and textures, including gulab jamun, jalebi, barfi, and rasgulla, satisfying sweet cravings with every bite.",    },
     {
       id: 8,
       image: "../images/rajmarice.jpg",
       name: "Rajma Rice",
       category: "lunch",
-      price: "60₹",
+      price: "₹200",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"Rajma rice is a delectable North Indian dish comprising red kidney beans cooked in a rich tomato-based gravy, served alongside fluffy steamed rice for a wholesome and satisfying meal.",    },
     {
       id: 9,
       image: "../images/samosa.jpg",
-      name: "samaso",
+      name: "Samosa",
       category: "evening",
-      price: "10₹",
+      price: "₹20",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"Samosa, a popular Indian snack, features a crispy, triangular pastry filled with spiced potatoes, peas, and sometimes meat. It's a savory treat enjoyed with chutney or as part of a meal.",    },
     {
       id: 10,
-      image: "../images/splmeal.jpg",
+      image: "../images/naan.jpg",
       name: "Special Meal",
       category: "splmeal",
-      price: "10₹",
+      price: "₹300",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
+"Naan tadka is a delectable Indian bread dish, where soft and fluffy naan is topped with a flavorful tadka of spices, herbs, and sometimes garlic. It's a delightful accompaniment to curries and gravies.",    },
   ];
   
   export default Menu;
